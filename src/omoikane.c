4e108c1571150876cac2335d2d04ed32c4b1922a
anna <anna@superpozycja.net>
2025-05-16 22:03:47 +0200

    version: add a version counter, just in case
    Signed-off-by: anna <anna@superpozycja.net>

--- src/omoikane.c
+++ src/omoikane.c
@@ -3,11 +3,13 @@
  */
 
 #include "logger.h"
+#include "version.h"
 
 int main(int argc, char *argv[])
 {
 	set_loglevel(INFO);
-	log(INFO, "hello from %s", argv[0]);
+	log(INFO, "hello from %s %d.%d.%d", argv[0],
+	    VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
 	return 0;
 }
---
+++ src/version.h
@@ -0,0 +1,10 @@
+#ifndef VERSION_H_1e2af3c5
+#define VERSION_H_1e2af3c5
+
+/* this changes in compliance with https://semver.org/ */
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+
+#endif
