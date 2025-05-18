a0a1b1e373b3067e6a52ea6809dcb0e52c3bec39
anna <anna@superpozycja.net>
2025-05-19 00:31:35 +0200

    omoikane: add state initialization in main
    Signed-off-by: anna <anna@superpozycja.net>

--- src/omoikane.c
+++ src/omoikane.c
@@ -6,12 +6,16 @@
 
 #include "logger.h"
 #include "version.h"
+#include "tile.h"
+#include "game_state.h"
 
 int main(int argc, char *argv[])
 {
 	char *level = getenv("LOGLEVEL");
 	if (level)
 		set_loglevel(level);
+
+	game_state_init();
 
 	log(INFO, "hello from %s %d.%d.%d", argv[0],
 	    VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
